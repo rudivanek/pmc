ff741fec777b58f63d73dc6d6632f9965a62cdc2
rudivanek <rfv@datago.net>
2025-09-12 20:12:37 -0600

    Fix restyleCopyWithPersona targetWordCount argument type

--- src/components/CopyMakerTab.tsx
+++ src/components/CopyMakerTab.tsx
@@ -475,7 +475,7 @@ const CopyMakerTab: React.FC<CopyMakerTabProps> = ({
           formState.model,
           formState.language,
           formState,
-          targetWordCount,
+          targetWordCount.target,
           addProgressMessage
         );
 
