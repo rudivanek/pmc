6e0ddadd39ad80e72a3af86dd17907a3cb7e226b
rudivanek <rfv@datago.net>
2025-09-22 13:50:29 -0600

    Remove duplicate displayMode props from CopyMakerForm

--- src/components/CopyMakerTab.tsx
+++ src/components/CopyMakerTab.tsx
@@ -843,8 +843,6 @@ const CopyMakerTab: React.FC<CopyMakerTabProps> = ({
             isPrefillEditingMode={isPrefillEditingMode}
             displayMode={displayMode}
             setDisplayMode={setDisplayMode}
-            displayMode={displayMode}
-            setDisplayMode={setDisplayMode}
           />
           
           {/* Prefill Action Buttons */}
