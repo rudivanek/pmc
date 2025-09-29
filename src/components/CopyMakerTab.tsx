3aba9a51f7cafb410d373f9ff156aaf904e12b96
rudivanek <rfv@datago.net>
2025-09-29 11:51:12 -0600

    Updated CopyMakerTab.tsx

--- src/components/CopyMakerTab.tsx
+++ src/components/CopyMakerTab.tsx
@@ -1329,9 +1329,6 @@ export default CopyMakerTab;
 
 
 
-
-
-
 {/* Load Template Section */}
             <div className="mb-4 sm:mb-6 p-3 sm:p-4 bg-gray-50 dark:bg-gray-900/20 border border-gray-200 dark:border-gray-800 rounded-lg">
               <div className="flex items-center justify-between mb-1">
