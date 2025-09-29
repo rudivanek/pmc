cfba872f603dfc6ef6c288d9859058fa5879ac85
rudivanek <rfv@datago.net>
2025-09-29 10:49:50 -0600

    Updated CopyMakerTab.tsx

--- src/components/CopyMakerTab.tsx
+++ src/components/CopyMakerTab.tsx
@@ -1150,7 +1150,7 @@ const CopyMakerTab: React.FC<CopyMakerTabProps> = ({
             <div className="mb-4 sm:mb-6 p-3 sm:p-4 bg-gray-50 dark:bg-gray-900/20 border border-gray-200 dark:border-gray-800 rounded-lg">
              
               Quick Start Templates
-            </h4>
+        </h4>
              
               <div className="mb-4">
                 <select
